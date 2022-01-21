2a45fb8105927ada3d93a931de7202b66e36472f
phuong <dieuphuong272@example.com>
2022-01-21 16:36:24 +0700

    CRUD news

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,10 +10,18 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the 'web' middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('admin/', 'PostController@index')->name('post.admin');
+Route::get('admin/create/', 'PostController@create')->name('post.create');
+Route::post('admin/store', [PostController::class, 'store'])->name('post.store');
+Route::get('admin/show/{id}', [PostController::class, 'show'])->name('post.show');
+Route::get('admin/edit/{post_id}', [PostController::class, 'edit'])->name('post.edit');
+Route::put('admin/update/{id}', [PostController::class, 'update'])->name('post.update');
+Route::delete('admin/delete/{id}', [PostController::class, 'destroy'])->name('post.destroy');
+Route::delete('admin/deleteAll', [PostController::class, 'deleteAll'])->name('post.deleteAll');
