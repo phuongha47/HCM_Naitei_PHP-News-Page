c60f10a0945f3e206cc41d56f412db5c8d46a666
phuong <dieuphuong272@example.com>
2022-02-17 19:33:19 +0700

    N

--- tests/Unit/Models/PostTest.php
+++ tests/Unit/Models/PostTest.php
@@ -24,35 +24,38 @@ class PostTest extends TestCase
     // check columns
     public function test_post_database_has_expected_columns()
     {
-        $this->assertTrue(Schema::hasColumns('posts', [
+        $this->assertTrue(Schema::hasColumns('posts',
+            [
             'id', "title", "body", "author_id", "category_id",
-          ]), 1);
+            ]
+        ), 1);
     }
     public function test_post_belongs_to_category()
     { 
-        $category = Category::factory()->create();
+        $category = Category::factory()->make();
         $post = Post::factory()->create(['category_id' => $category->id]);
+        
         $this->assertInstanceOf(Category::class, $post->category);
         $this->assertEquals('category_id', $post->category()->getForeignKeyName());
     }
-    public function test_post_has_many_images()
-    {
-        $category = Category::factory()->create();
-        $post = Post::factory()->create(['category_id' => $category->id]);
-        $images = Image::factory()->create(['imageable_id' => $post->id, 'imageable_type' => Post::class]); 
-        // kiểm tra foreignkey
-        $this->assertEquals('category_id', $category->posts()->getForeignKeyName());
-        //posts related category
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $category->posts);
-    }
-    public function test_post_has_many_comments()
-    {
-        $category = Category::factory()->create();
-        $post = Post::factory()->create(['category_id' => $category->id]);
-        $images = Image::factory()->create(['imageable_id' => $post->id, 'imageable_type' => Post::class]); 
-        // kiểm tra foreignkey
-        $this->assertEquals('category_id', $category->posts()->getForeignKeyName());
-        //posts related category
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $category->posts);
-    }
+    // public function test_post_has_many_images()
+    // {
+    //     $category = Category::factory()->create();
+    //     $post = Post::factory()->create(['category_id' => $category->id]);
+    //     $images = Image::factory()->create(['imageable_id' => $post->id, 'imageable_type' => Post::class]); 
+    //     // kiểm tra foreignkey
+    //     $this->assertEquals('category_id', $category->posts()->getForeignKeyName());
+    //     //posts related category
+    //     $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $category->posts);
+    // }
+    // public function test_post_has_many_comments()
+    // {
+    //     $category = Category::factory()->create();
+    //     $post = Post::factory()->create(['category_id' => $category->id]);
+    //     $images = Image::factory()->create(['imageable_id' => $post->id, 'imageable_type' => Post::class]); 
+    //     // kiểm tra foreignkey
+    //     $this->assertEquals('category_id', $category->posts()->getForeignKeyName());
+    //     //posts related category
+    //     $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $category->posts);
+    // }
 }
